c9fb0ccf9a0eb2efe7bc201cc63fedbc48e336ab
AlisherShalken <200103162@stu.sdu.edu.kz>
2022-12-04 18:01:24 +0600

    functios for creating and logging user

--- app/src/main/java/com/example/appchat/Login.kt
+++ app/src/main/java/com/example/appchat/Login.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
 
@@ -13,11 +16,13 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
         btnLogin = findViewById(R.id.btnLogin)
@@ -27,5 +32,16 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password =  edtPassword.text.toString()
+
+            login(email,password);
+        }
+    }
+
+    private fun login(email: String, password: String){
+    // login for logging user
     }
 }
